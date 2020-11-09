3a385ba8ba134ea91758dcfc261394a7d3bd9016
Usama <umalik@academytrainee.com>
2020-11-09 12:51:46 +0000

    updated project structure

---
+++ main/java/com/qa/qacommunity/Runner.java
@@ -0,0 +1,17 @@
+package com.qa.qacommunity;
+
+import arraysTask.*;
+import conditionals.*;
+import enhancedForLoops.*;
+import iteration.*;
+import operators.Ex1_Calculator;
+import operators.Ex2_Result;
+import person_task.Person;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Ex2_Result.init();
+	}
+
+}
